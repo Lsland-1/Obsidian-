98d77ce78497c17ddcce842201b955f17f41d11f
Lsland <2475572453@qq.com>
2024-10-17 18:11:09 +0800

    10.17

--- QT常用/渲染控件加载图像.ts
+++ QT常用/渲染控件加载图像.ts
@@ -10,3 +10,39 @@ void* renderCtrlParamData = renderCtrlCircleTool->GetParamObj()->GetControlInfo(
 qlonglong renderCtrlPData = (qlonglong)renderCtrlParamData;
 ui->axWidgetParam->dynamicCall("SetParamsInfo(qlonglong)", renderCtrlPData);
 
+//线线测量渲染
+IMVSL2LMeasureModuTool * pL2LMeasureObject = (IMVSL2LMeasureModuTool *)(*m_pVmSol)["流程1.线线测量1"];
+void* renderCtrlParamData = pL2LMeasureObject->GetParamObj()->GetControlInfo();
+qlonglong renderCtrlPData = (qlonglong)renderCtrlParamData;
+ui->axWidgetParam->dynamicCall("SetParamsInfo(qlonglong)", renderCtrlPData);
+
+//圆圆测量渲染
+IMVSC2CMeasureModuTool * pC2CMeasureObject = (IMVSC2CMeasureModuTool *)(*m_pVmSol)["流程1.圆圆测量1"];
+void* renderCtrlParamData = pC2CMeasureObject->GetParamObj()->GetControlInfo();
+qlonglong renderCtrlPData = (qlonglong)renderCtrlParamData;
+ui->axWidgetParam->dynamicCall("SetParamsInfo(qlonglong)", renderCtrlPData);
+
+//点点测量渲染
+IMVSP2PMeasureModuTool * pP2PMeasureObject = (IMVSP2PMeasureModuTool *)(*m_pVmSol)["流程1.点点测量1"];
+void* renderCtrlParamData = pP2PMeasureObject->GetParamObj()->GetControlInfo();
+qlonglong renderCtrlPData = (qlonglong)renderCtrlParamData;
+ui->axWidgetParam->dynamicCall("SetParamsInfo(qlonglong)", renderCtrlPData);
+
+//线圆测量渲染
+IMVSL2CMeasureModuTool * pL2CMeasureObject = (IMVSL2CMeasureModuTool *)(*m_pVmSol)["流程1.线圆测量1"];
+void* renderCtrlParamData = pL2CMeasureObject->GetParamObj()->GetControlInfo();
+qlonglong renderCtrlPData = (qlonglong)renderCtrlParamData;
+ui->axWidgetParam->dynamicCall("SetParamsInfo(qlonglong)", renderCtrlPData);
+
+//点圆测量渲染
+IMVSP2CMeasureModuTool * pP2CMeasureObject = (IMVSP2CMeasureModuTool *)(*m_pVmSol)["流程1.点圆测量1"];
+void* renderCtrlParamData = pP2CMeasureObject->GetParamObj()->GetControlInfo();
+qlonglong renderCtrlPData = (qlonglong)renderCtrlParamData;
+ui->axWidgetParam->dynamicCall("SetParamsInfo(qlonglong)", renderCtrlPData);
+
+//点线测量渲染
+IMVSP2LMeasureModuTool * pP2LMeasureObject = (IMVSP2LMeasureModuTool *)(*m_pVmSol)["流程1.点线测量1"];
+void* renderCtrlParamData = pP2LMeasureObject->GetParamObj()->GetControlInfo();
+qlonglong renderCtrlPData = (qlonglong)renderCtrlParamData;
+ui->axWidgetParam->dynamicCall("SetParamsInfo(qlonglong)", renderCtrlPData);
+
